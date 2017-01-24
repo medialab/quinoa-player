1ce91946d2c28d84d73b2879c326ecba6a270598
Robin de Mourat <robin.demourat@gmail.com>
2017-01-24 09:22:12 +0100

    update stories

--- stories/index.js
+++ stories/index.js
@@ -3,26 +3,6 @@ import { storiesOf, action, linkTo } from '@kadira/storybook';
 import Welcome from './Welcome';
 import Player from '../src/Player';
 
-
-import ultrasimple from './mocks/ultrasimple_presentation.json';
-
-storiesOf('Welcome', module)
-  .add('to Storybook', () => (
-    <Welcome showApp={linkTo('Button')}/>
-  ));
-
-storiesOf('Quinoa presentation player', module)
-  .add('with very simple presentation', () => (
-    <Player presentation={ultrasimple} />
-  ))
-  .add('empty', () => (
-    <Player />
-  ))
-  .add('with invalid data', () => (
-    <Player presentation={{}} />
-  ));
-
-
 import mapPresentation from './map/simple-map-presentation.json';
 storiesOf('Quinoa map presentation', module)
   .add('Cartel mode, navigable', () => (
